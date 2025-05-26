d814012082de24869f4f160e5360d07f1ab79edc
vortecs <vortecs-one@protonmail.com>
2025-05-26 20:49:25 +0000

    bcript script hash generator

--- bcrypt-hash.js
+++ bcrypt-hash.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 // Hash password
-const password = 'example_password'; // Use your desired plain text password
+const password = 'password'; // Use your desired plain text password
 const hashedPassword = bcrypt.hashSync(password, 10);
 
 console.log('Hashed Password:', hashedPassword);
--- script.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-// Hash password
-const password = 'chetu2025'; // Use your desired plain text password
-const hashedPassword = bcrypt.hashSync(password, 10);
-
-console.log('Hashed Password:', hashedPassword);
-
