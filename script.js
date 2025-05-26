54f9a5975e601e79a7d1d7a2a7d193a512c45525
vortecs-one <95098576+vortecs-one@users.noreply.github.com>
2025-05-26 16:24:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 
 // Hash password
-const password = 'chetu2025'; // Use your desired plain text password
+const password = 'example_password'; // Use your desired plain text password
 const hashedPassword = bcrypt.hashSync(password, 10);
 
 console.log('Hashed Password:', hashedPassword);
